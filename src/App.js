991c1ce1ccf1a7cc4643c0dbcee461abef1e9789
erkinov26 <erkinov0247@gmail.com>
2023-10-09 03:26:37 +0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialItems = Array(9).fill(null);
+  const [clickedItems, setClickedItems] = useState(initialItems);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winnnerInfo, setWinnnerInfo] = useState(null);
+  const [gameState, setGameState] = useState("inProgress"); // Initially, the game is in progress
+  console.log(clickedItems);
+
+  function winner() {
+    let winCombination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winCombination.forEach((e) => {
+      if (
+        clickedItems[e[0]] === clickedItems[e[1]] &&
+        clickedItems[e[1]] === clickedItems[e[2]] &&
+        clickedItems[e[2]]
+      ) {
+        setTimeout(() => {
+          setGameState("over");
+          setWinnnerInfo(currentPlayer === "X" ? "O is winner" : "X is winner");
+        }, 100);
+      }
+    });
+  }
+  winner();
+  const handleClick = (index) => {
+    if (clickedItems[index] === null) {
+      const newClickedItems = [...clickedItems];
+      newClickedItems[index] = currentPlayer;
+      setClickedItems(newClickedItems);
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+      setWinnnerInfo(null);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="gameTitle"> X And O Game</h1>
+      <div className="gameBox">
+        {clickedItems.map((item, index) => (
+          <button
+            key={index}
+            className={`box ${item ? `clicked${item}` : ""} ${
+              currentPlayer === "X" &&
+              item === null &&
+              gameState === "inProgress"
+                ? "xHover"
+                : currentPlayer === "O" &&
+                  item === null &&
+                  gameState === "inProgress"
+                ? "oHover"
+                : undefined
+            }`}
+            disabled={item || gameState === "over"}
+            onClick={() => handleClick(index)}
+          ></button>
+        ))}
+      </div>
+      {gameState === "over" && (
+        <div>
+          <h1 className="title">{winnnerInfo}</h1>
+          <button
+            className={"btn btn-secondary"}
+            onClick={() => {
+              setClickedItems(Array(9).fill(null));
+              setCurrentPlayer("X");
+              setGameState("inProgress");
+            }}
+          >
+            Restart
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/styleComponents.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const BoxDiv = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 5px solid #00ffff;
+  background-color: #00ffff;
+  border-radius: 8px;
+  
+`;
+export default BoxDiv;
